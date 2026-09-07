85722af9d7b12b115130721dc08b85277fa979b5
AdityaP183 <adityaprasad1828@gmail.com>
2026-09-07 16:58:16 +0530

    feat(users): add account deletion requests

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,8 +1,14 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
 import { response } from "../../utils/response.js";
 import { HTTP_STATUS } from "../../constants/http.js";
-import { getUserById, handleUpdateUser } from "./users.service.js";
+import {
+    getUserById,
+    handleGetAllUserDeletionRequests,
+    handleUpdateUser,
+    handleUserDeletionRequest,
+} from "./users.service.js";
 import { updateUserSchema } from "./users.schema.js";
+import { randomUUID } from "node:crypto";
 
 async function getCurrentUser(req: FastifyRequest, res: FastifyReply) {
     const userId = req.userId;
@@ -31,4 +37,42 @@ async function updateUserInfo(req: FastifyRequest, res: FastifyReply) {
     );
 }
 
-export { getCurrentUser, updateUserInfo };
+async function userDeletionRequest(req: FastifyRequest, res: FastifyReply) {
+    const userId = req.userId;
+    const userDeletionTableId = randomUUID();
+
+    const deletionRequest = await handleUserDeletionRequest(
+        userId,
+        userDeletionTableId,
+    );
+
+    return response.success(
+        res,
+        HTTP_STATUS.CREATED,
+        "Account deletion request submitted successfully!",
+        deletionRequest,
+    );
+}
+
+async function getAllUserDeletionRequests(
+    req: FastifyRequest,
+    res: FastifyReply,
+) {
+    const userId = req.userId;
+
+    const deletionRequests = await handleGetAllUserDeletionRequests(userId);
+
+    return response.success(
+        res,
+        HTTP_STATUS.CREATED,
+        "Account deletion request submitted successfully!",
+        deletionRequests,
+    );
+}
+
+export {
+    getCurrentUser,
+    updateUserInfo,
+    userDeletionRequest,
+    getAllUserDeletionRequests,
+};
--- src/modules/users/users.routes.ts
+++ src/modules/users/users.routes.ts
@@ -1,10 +1,25 @@
 import type { FastifyPluginAsync } from "fastify";
 import authorizedPass from "../../middlewares/auth.js";
-import { getCurrentUser, updateUserInfo } from "./users.controller.js";
+import {
+    getAllUserDeletionRequests,
+    getCurrentUser,
+    updateUserInfo,
+    userDeletionRequest,
+} from "./users.controller.js";
 
 const usersRoutes: FastifyPluginAsync = async (fastify) => {
     fastify.get("/me", { preHandler: authorizedPass }, getCurrentUser);
     fastify.patch("/me", { preHandler: authorizedPass }, updateUserInfo);
+    fastify.get(
+        "/me/deletion-request",
+        { preHandler: authorizedPass },
+        getAllUserDeletionRequests,
+    );
+    fastify.post(
+        "/me/deletion-request",
+        { preHandler: authorizedPass },
+        userDeletionRequest,
+    );
 };
 
 export default usersRoutes;
